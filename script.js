a002937a114841ef23e08934ad87d92153f267fe
Xoli <xolinxiweni@gmail.com>
2024-03-13 10:24:00 +0200

    edits

--- script.js
+++ script.js
@@ -92,22 +92,22 @@ if(navigator.geolocation){
                     climate.textContent=main;
                     tempvalue.textContent=Math.round(feels_like-273);
                     if(id<300 && id>200){
-                        tempicon.src="./icons/thunderstorm.svg"
+                        tempicon.src="icons/thunderstorm.svg"
                     }
                    else  if(id<400 && id>300){
-                        tempicon.src="./icons/cloud-solid.svg"
+                        tempicon.src="icons/cloud-solid.svg"
                     }
                    else if(id<600&& id>500){
-                        tempicon.src="./icons/rain.svg"
+                        tempicon.src="icons/rain.svg"
                     }
                    else  if(id<700 && id>600){
-                        tempicon.src="./icons/snow.svg"
+                        tempicon.src="icons/snow.svg"
                     }
                    else  if(id<800 && id>700){
-                        tempicon.src="./icons/clouds.svg"
+                        tempicon.src="icons/clouds.svg"
                     }
                      else if(id==800){
-                        tempicon.src="./icons/clouds-and-sun.svg"
+                        tempicon.src="icons/clouds-and-sun.svg"
                     }
                     console.log(data);
             })
