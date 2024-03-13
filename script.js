393e3bffb236a4e56aed7198ea7c2eae49a99afc
Xoli <xolinxiweni@gmail.com>
2024-03-13 10:19:06 +0200

    edits

--- script.js
+++ script.js
@@ -6,20 +6,15 @@ let iconfile;
 const searchInput=document.getElementById("search-input");
 const searchButton=document.getElementById("search-button");
 
-searchButton.addEventListener('click', (e)=>
-{
-
+searchButton.addEventListener('click', (e)=>{
 e.preventDefault();
 getWeather(searchInput.value);
 searchInput.value='';
-
-
 });
 
 
 
-const getWeather=async (city)=>
-{
+const getWeather=async (city)=>{
     try{
 
         const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=515a6094aa66db1b7397a4db433855cb`,
@@ -64,59 +59,30 @@ const getWeather=async (city)=>
 
 
     }
-catch(error)
-{
+catch(error){
     alert('city not found');
-}
+}};
 
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener("load" ,()=>{
+window.addEventListener("load", ()=>{
 
 let long;
 let lat;
 
-if(navigator.geolocation)
-{
-
-    navigator.geolocation.getCurrentPosition((position)=>
-    {
-
-
-
-    long=position.coords.longitude;
-    lat=position.coords.latitude;
-    const proxy="https://cors-anywhere.herokuapp.com/";
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position)=>{
+        long=position.coords.longitude;
+        lat=position.coords.latitude;
+        const proxy="https://cors-anywhere.herokuapp.com/";
 
         const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=dab3af44de7d24ae7ff86549334e45bd     `
 
         fetch(api).then((response)=>{
 
             return response.json();
-
-
         })
 
-        .then (data =>
-            {
-
+        .then (data =>{
+        
                     const{name}=data;
                     const{feels_like}=data.main;
                     const{id,main}=data.weather[0];
@@ -125,47 +91,26 @@ if(navigator.geolocation)
                     loc.textContent=name;
                     climate.textContent=main;
                     tempvalue.textContent=Math.round(feels_like-273);
-                    if(id<300 && id>200)
-                    {
+                    if(id<300 && id>200){
                         tempicon.src="./icons/thunderstorm.svg"
                     }
-                   else  if(id<400 && id>300)
-                    {
+                   else  if(id<400 && id>300){
                         tempicon.src="./icons/cloud-solid.svg"
                     }
-                   else if(id<600&& id>500)
-                    {
+                   else if(id<600&& id>500){
                         tempicon.src="./icons/rain.svg"
                     }
-                   else  if(id<700 && id>600)
-                    {
+                   else  if(id<700 && id>600){
                         tempicon.src="./icons/snow.svg"
                     }
-                   else  if(id<800 && id>700)
-                    {
+                   else  if(id<800 && id>700){
                         tempicon.src="./icons/clouds.svg"
                     }
-                     else if(id==800)
-                    {
+                     else if(id==800){
                         tempicon.src="./icons/clouds-and-sun.svg"
                     }
-
-
-
-
-
                     console.log(data);
-
-
             })
-
-
-
-}
-
-
-
+        }
     )}
-
-
 })
